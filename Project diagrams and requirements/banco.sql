229c1ed074618f6d6103e04f7cd47d057114c6d4
ThiagoS332 <13428676+ThiagoS332@users.noreply.github.com>
2021-05-28 03:33:44 -0300

    Atualização do Banco de Dados

--- Project diagrams and requirements/banco.sql
+++ Project diagrams and requirements/banco.sql
@@ -1,8 +1,12 @@
-ALTER TABLE OrderReport
-ADD orderReport_id int,
-ADD orderReport_piece_description varchar(1020),
-ADD orderReport_price decimal,
-ADD PRIMARY KEY (orderReport_id),
-ADD FOREIGN KEY (technician_id) REFERENCES Technician(technician_id),
-ADD FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-ADD FOREIGN KEY (serviceCost_id) REFERENCES ServiceCost(serviceCost_id);
+INSERT INTO OrderReport (orderReport_id, orderReport_piece_description, orderReport_price, technician_id, order_id, serviceCost_id)
+VALUES
+	(000000001, 'problema na memóri ram e nos cabos da fonte', 108.64, 000000001, 000000002, 000000001),
+	(000000002, 'soldagem dos cabos', 62.96, 000000003, 000000003, 000000002),
+	(000000003, 'troca de cooler', 106.96, 000000004, 000000004, 000000003),
+	(000000004, 'troca de pasta térmica', 37.65, 000000003, 000000005, 000000004),
+	(000000005, 'problema no hd', 180.53, 000000001, 000000007, 000000005),
+	(000000006, 'limpeza', 82.24, 000000004, 000000009, 000000006),
+	(000000007, 'placa mãe queimada', 315.63, 000000001, 000000011, 000000007),
+	(000000008, 'montagem de componentes do pc', 83.48, 000000003, 000000013, 000000008);
+	
+SELECT * FROM OrderReport
